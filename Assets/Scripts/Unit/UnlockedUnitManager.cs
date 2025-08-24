923f0bebc7bb59a7469955d6449772ce5344efa5
25Imada <imadatomoki@gmail.com>
2025-08-24 23:51:26 +0900

    バーチャルマウス改善、アイコンリスト作成、データベース用意

---
+++ Assets/Scripts/Unit/UnlockedUnitManager.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnitManager : MonoBehaviour
+{
+	[SerializeField] private UnitDatabase database;
+	[SerializeField] private int initialUnlockedUnits = 16;
+	static public List<UnitData> unlockedUnits = new List<UnitData>();
+
+	void Start()
+	{
+		// 初期状態：データベースの先頭から解禁する
+		for (int i = 0; i < initialUnlockedUnits; i++)
+		{
+			if (i < database.allUnits.Count)
+			{
+				UnlockUnit(database.allUnits[i]);
+			}
+		}
+	}
+
+	static public void UnlockUnit(UnitData newUnit)
+	{
+		if (!unlockedUnits.Contains(newUnit))
+		{
+			unlockedUnits.Add(newUnit);
+			// Debug.Log(newUnit.unitName + " を解禁しました！");
+		}
+	}
+}
