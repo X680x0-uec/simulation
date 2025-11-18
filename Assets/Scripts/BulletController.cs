9c733eb9f39b363f4fe5bad8061aa41aae8bb75f
ankocpy <k2311089@gl.cc.uec.ac.jp>
2025-11-18 15:15:49 +0900

    ranged enemy controllerとbullet controllerを修正(他の敵と同じように処理ができるようになったはず)

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,106 +1,16 @@
-using UnityEngine;
 using System.Collections;
-using Unity.VisualScripting;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class BulletController : MonoBehaviour
+public class BulletController : EnemyController
 {
-    [Header("基本設定")]
-    [SerializeField] private int maxHP = 100;
-    private int currentHP;
-
-    [Header("追尾設定")]
-    [SerializeField] private float speed = 2f;
-    [SerializeField] private GameObject mikoshiObject;
-    [SerializeField] private float giveUpTime = 2f; // ミコシに近づけなくなったら諦める時間
-    private float giveUpTimer;
-    private float distanceToMikoshi;
-
-    private Vector2 currentPosition;
-    private Vector2 targetPosition;
-
-    [Header("消滅設定")]
-    [SerializeField] private float destroyXPosition = 5f; // ミコシからのX距離がこれ以上離れたら消滅
-
-    private Rigidbody2D rb;
-    private bool canMove = true;
-
-    void Awake()
+    protected override IEnumerator Knockback()
     {
-        currentHP = maxHP;
-        giveUpTimer = giveUpTime;
-        
-        rb = GetComponent<Rigidbody2D>();
-        if (rb == null) Debug.LogError("EnemyController に Rigidbody2D が必要じゅう！");
-        if (mikoshiObject == null)
-        {
-            mikoshiObject = GameObject.FindWithTag("Mikoshi");
-        }
+        Destroy(gameObject);
+        yield break;
     }
-
-    void Update()
+    protected override void ExecuteAI()
     {
-        if (mikoshiObject != null && canMove)
-        {
-            FollowTarget();
-        }
-
-        // ミコシから一定距離以上離れたら消滅
-        if (targetPosition.x - currentPosition.x > destroyXPosition)
-        {
-            Destroy(gameObject);
-        }
+        FollowTarget(mikoshiObject);
     }
-
-    void FollowTarget()
-    {
-        currentPosition = transform.position;
-        targetPosition = mikoshiObject.GetComponent<Transform>().position;
-        Vector2 direction = (targetPosition - currentPosition).normalized;
-
-        rb.AddForce(direction * speed);
-
-        // ミコシに近づけなくなったら一定時間後に諦める
-        if (distanceToMikoshi <= Vector2.Distance(currentPosition, targetPosition))
-        {
-            giveUpTimer -= Time.deltaTime;
-            if (giveUpTimer <= 0f)
-            {
-                // 一定時間後に諦める
-                canMove = false;
-            }
-        }
-        else
-        {
-            giveUpTimer = giveUpTime; // リセット
-        }
-        distanceToMikoshi = Vector2.Distance(currentPosition, targetPosition);
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Mikoshi"))
-        {
-            // --- ① ダメージを与える ---
-            // MikoshiController.mikoshiHP -= 10;
-
-            Destroy(gameObject);
-        }
-    }
-
-
-    public void TakeDamage(int damage, Vector3 attackerPosition)
-    {
-        // --- ① ダメージ処理 ---
-        // （ここでは単純にログを出すだけ）
-        currentHP -= damage;
-        Debug.Log(gameObject.name + "は" + damage + "のダメージを受けたじゅう！ 残りHP: " + currentHP);
-        if (currentHP <= 0)
-        {
-            Debug.Log(gameObject.name + "は倒れたじゅう！");
-            Destroy(gameObject);
-            return;
-        }
-    }
-    
-
 }
