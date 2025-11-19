4024ce2cc4becfb3638e6eff90b8167969d8a9b8
25Imada <imadatomoki@gmail.com>
2025-11-20 00:07:35 +0900

    コントローラー完全対応

--- Assets/Scripts/UI/VirtualMousePositionAdjuster.cs
+++ Assets/Scripts/UI/VirtualMousePositionAdjuster.cs
@@ -3,21 +3,37 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.UI;
 using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.SceneManagement;
 
-public class SoftwareCursorPositionAdjuster : MonoBehaviour
+public class VirtualMousePositionAdjuster : MonoBehaviour
 {
     [SerializeField] private VirtualMouseInput _virtualMouseInput;
+    [SerializeField] private RectTransform cursorTransform;
     [SerializeField] private InputSystemUIInputModule _inputSystemUIInputModule;
     [SerializeField] private Canvas _canvas;
 
     private Mouse virtualMouse;
     private float _lastScaleFactor = 1;
+    
+    public bool isActive = false;
 
     // 現在のCanvasスケール
     private float CurrentScale =>
         _virtualMouseInput.cursorMode == VirtualMouseInput.CursorMode.HardwareCursorIfAvailable
             ? 1
             : _canvas.scaleFactor;
+
+    private void OnEnable()
+    {
+        if (SceneManager.GetActiveScene().name == "TitleScene")
+        {
+            isActive = true;
+        }
+        else
+        {
+            isActive = false;
+        }
+    }
 
     // Canvasのスケールを監視して、VirtualMouseの座標を補正する
     private void Update()
@@ -31,11 +47,22 @@ public class SoftwareCursorPositionAdjuster : MonoBehaviour
         }
         else
         {
-            // 画面外に行かないように補正
-            Vector2 virtualMousePosition = virtualMouse.position.ReadValue();
-            virtualMousePosition.x = Mathf.Clamp(virtualMousePosition.x, 0, _canvas.pixelRect.width);
-            virtualMousePosition.y = Mathf.Clamp(virtualMousePosition.y, 0, _canvas.pixelRect.height);
-            InputState.Change(virtualMouse.position, virtualMousePosition);
+            if (!isActive)
+            {
+                Vector2 offScreenPos = new Vector2(-9999, -9999);
+                InputState.Change(virtualMouse.position, offScreenPos);
+                if (cursorTransform != null)
+                    cursorTransform.anchoredPosition = offScreenPos;
+                return;
+            }
+            else
+            {
+                // 画面外に行かないように補正
+                Vector2 virtualMousePosition = virtualMouse.position.ReadValue();
+                virtualMousePosition.x = Mathf.Clamp(virtualMousePosition.x, 0, _canvas.pixelRect.width);
+                virtualMousePosition.y = Mathf.Clamp(virtualMousePosition.y, 0, _canvas.pixelRect.height);
+                InputState.Change(virtualMouse.position, virtualMousePosition);
+            }
         }
 
         // スケールが変化した時のみ、以降の処理を実行
